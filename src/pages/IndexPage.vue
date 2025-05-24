96251865d793a91712693d914b4d23d2842007f6
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2025-05-24 17:29:36 -0400

    feat:  💫  add pages home

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,89 @@
 <template>
-  <div class="text-h6 q-mb-md">Título da página</div>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <div>
+    <div class="text-h6 q-mb-md">Título da Página</div>
+    <div>
+      <q-input
+        v-model="searchTerm"
+        @keyup.enter="searchGifs"
+        placeholder="Pesquisar GIFs"
+        clearable
+      />
+      <q-btn @click="searchGifs" label="Buscar" />
+    </div>
+    <q-page class="grid-container">
+      <div v-if="gifs.length === 0">Carregando GIFs...</div>
+      <div v-else class="grid">
+        <div v-for="gif in gifs" :key="gif.id" class="gif-container">
+          <img :src="gif.images.fixed_height.url" :alt="gif.title" height="200" />
+          <q-btn
+            @click="() => store.addToFavoritos(gif)"
+            label="Favoritar"
+            class="favorite-button"
+          />
+        </div>
+      </div>
+    </q-page>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useGiphyStore } from './../stores/useGiphyStore.store';
+import { storeToRefs } from 'pinia';
+
+const store = useGiphyStore();
+const searchTerm = ref('');
+
+const { gifs } = storeToRefs(store);
+
+const fetchGifs = async () => {
+  try {
+    await store.fetchGifs(true);
+    console.log('Response:', gifs);
+  } catch (error) {
+    console.error('Erro ao buscar GIFs:', error);
+  }
+};
+
+const searchGifs = async () => {
+  try {
+    if (searchTerm.value) {
+      await store.fetchGifs(false, searchTerm.value);
+    } else {
+      await fetchGifs();
+    }
+  } catch (error) {
+    console.error('Erro ao pesquisar GIFs:', error);
+  }
+};
+
+onMounted(async () => {
+  await fetchGifs();
+});
+</script>
+
+<style scoped>
+.grid-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 10px;
+  width: 100%;
+}
+
+.gif-container {
+  text-align: center;
+}
+img {
+  max-width: 200px;
+}
+.favorite-button {
+  display: block;
+  margin: 10px auto;
+}
+</style>
